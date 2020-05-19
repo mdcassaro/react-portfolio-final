c010c6fcc0f2581d67bcca979ee16b4f613274f5
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-19 11:08:15 -0500

    Updates

---
+++ precache-manifest.44ac303434ebd6605d14cf61deb9826b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d3c0d0ad27fa402c23535a8b8c2cfc",
+    "url": "/react-portfolio-final/index.html"
+  },
+  {
+    "revision": "222c8eb8c9758910b9ed",
+    "url": "/react-portfolio-final/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c177d41868e47cd2618",
+    "url": "/react-portfolio-final/static/js/2.d5454790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio-final/static/js/2.d5454790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222c8eb8c9758910b9ed",
+    "url": "/react-portfolio-final/static/js/main.b7530f2e.chunk.js"
+  },
+  {
+    "revision": "37fbfe2d142d4884461a",
+    "url": "/react-portfolio-final/static/js/runtime-main.858add92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio-final/static/media/logo.5d5d9eef.svg"
+  }
+]);
