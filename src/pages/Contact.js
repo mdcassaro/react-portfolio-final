a79051d4d6fa25582290eef3f24203e2d568b922
Michael Cassaro <mdcassaro@hotmail.com>
2020-06-09 18:28:42 -0500

    contacted page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,29 @@
 import React from "react";
-import "../styles/Contact.css";
+import "../styles/contact.css";
+
 
 
 
 function Contact(){
     return(
-        <div class="container">
-        <div class="row" >
-            <div class="col-md-10">
-                <form>
-                    <div class="form-group">
-                      <label for="exampleFormControlInput1">My Linkedin Profile: </label>
-                      <a href="https://www.linkedin.com/in/michael-cassaro-5580161a6/" target="_blank" rel= "noopener noreferrer"class="btn btn-primary">Visit my Linkedin</a>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Email Me:</label>
-                        <a href="mailto:myemail@gmail.com" target="_blank" rel= "noopener noreferrer"  class="btn btn-primary">Click to Email Me</a>
-                    </div>
-                    
-                </form>
+        <div class="container align-self-center">
+            <h1>My Contact Information:</h1>
+        
+            <div className="row justify-content-around">
 
+                <a href="https://www.linkedin.com/in/michael-cassaro-5580161a6/" target="_blank" rel= "noopener noreferrer"class="btn btn-primary">Visit my Linkedin</a>
+            
+
+           
+
+            <a href="mailto:mdcassaro@hotmailmail.com" target="_blank" rel= "noopener noreferrer"  class="btn btn-primary">Click to Email Me</a>
+                   
+                
             </div>
-        </div>
+                    
+                
+
+           
       </div>
       )
     }
